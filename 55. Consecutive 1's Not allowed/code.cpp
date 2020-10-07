895c922c9be7136c6d7bb48a8e9cc850414108a4
Aman Jain <amangrand@gmail.com>
2020-10-08 01:30:43 +0530

    Update code.cpp

--- 55. Consecutive 1's Not allowed/code.cpp
+++ 55. Consecutive 1's Not allowed/code.cpp
@@ -3,14 +3,15 @@ public:
 	// #define ll long long
 	ll countStrings(int n) {
 	    const ll mod=1e9+7;
-	    ll dp[100005]={0};
-	    dp[1]=2;
-	    dp[2]=3;
-	    dp[3]=5;
-	    for(int i=4; i<=n ; i++)
+	    ll last1=1;
+	    ll last0=1;
+	    for(int i=2; i<=n ; i++)
 	    {
-	        dp[i]=(dp[i-1]+dp[i-2])%mod;
+	        ll newLast0=(last1+last0)%mod;
+	        ll newLast1=last0%mod;
+	        last1=newLast1;
+	        last0=newLast0;
 	    }
-	    return dp[n];
+	    return (last1+last0)%mod;
 	}
 };
