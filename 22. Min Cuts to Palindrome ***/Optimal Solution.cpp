34bd17e85dc9b691d44e4badd1a8fc92b8519e4b
Aman Jain <amangrand@gmail.com>
2021-04-04 22:31:22 +0530

    Update Optimal Solution.cpp

--- 22. Min Cuts to Palindrome ***/Optimal Solution.cpp
+++ 22. Min Cuts to Palindrome ***/Optimal Solution.cpp
@@ -1,28 +1,36 @@
- T: O(n^2)
- 
- 
- class Solution {
-    public:
-        int minCut(string s) {
-            if(s.empty()) return 0;
-            int n = s.size();
-            vector<vector<bool>> pal(n,vector<bool>(n,false));
-            vector<int> d(n);
-            for(int i=n-1;i>=0;i--)
-            {
-                d[i]=n-i-1;
-                for(int j=i;j<n;j++)
-                {
-                    if(s[i]==s[j] && (j-i<2 || pal[i+1][j-1]))
-                    {
-                       pal[i][j]=true;
-                       if(j==n-1)
-                           d[i]=0;
-                       else if(d[j+1]+1<d[i])
-                           d[i]=d[j+1]+1;
+int palindromicPartition(string s)
+{
+          if (s.empty())
+                    return 0;
+          int n = s.size();
+          vector<vector<bool>>dp(n, vector<bool>(n, false));
+          for (int gap = 0; gap < s.length() ; gap++) {
+                    for (int i = 0, j = gap; j < s.length(); i++, j++) {
+                              if (gap == 0) {
+                                        dp[i][j] = true;
+                              } else if (gap == 1) {
+                                        dp[i][j] = (s[i] == s[j]);
+                              } else {
+                                        dp[i][j] = (s[i] == s[j]) && (dp[i + 1][j - 1]);
+                              }
                     }
-                }
-            }
-            return d[0];
-        }
-    };
+          }
+         
+          int ans[n];
+          ans[0] = 0;
+          for (int i = 1; i < n ; i++) {
+                    if (dp[0][i]) {
+                              ans[i] = 0;
+                              continue;
+                    }
+                    ans[i] = INT_MAX;
+                    for (int j = i ; j > 0 ; j--) {
+                              if (dp[j][i]) {
+                                        if (ans[j - 1] + 1 < ans[i]) {
+                                                  ans[i] = ans[j - 1] + 1;
+                                        }
+                              }
+                    }
+          }
+          return ans[n - 1];
+}
